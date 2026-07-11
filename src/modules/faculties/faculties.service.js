cd7734d5b6122afb0c5c2a14cbb43f38fa3cd7d8
Isah Muhammad <alhajimuhammadisah@gmail.com>
2026-07-11 11:01:09 +0100

    Updated faculty service

--- src/modules/faculties/faculties.service.js
+++ src/modules/faculties/faculties.service.js
@@ -2,7 +2,24 @@ const facultiesRepository = require("./faculties.repository");
 
 class FacultiesService {
   async createFaculty(payload) {
-    return await facultiesRepository.create(payload);
+    try {
+      return await facultiesRepository.create(payload);
+    } catch (error) {
+      if (error.code === "23505") {
+        switch (error.constraint) {
+          case "faculties_code_key":
+            throw new Error("Faculty code already exists.");
+
+          case "faculties_name_key":
+            throw new Error("Faculty name already exists.");
+
+          default:
+            throw new Error("Faculty already exists.");
+        }
+      }
+
+      throw error;
+    }
   }
 
   async getAllFaculties() {
@@ -20,7 +37,24 @@ class FacultiesService {
   }
 
   async updateFaculty(id, payload) {
-    return await facultiesRepository.update(id, payload);
+    try {
+      return await facultiesRepository.update(id, payload);
+    } catch (error) {
+      if (error.code === "23505") {
+        switch (error.constraint) {
+          case "faculties_code_key":
+            throw new Error("Faculty code already exists.");
+
+          case "faculties_name_key":
+            throw new Error("Faculty name already exists.");
+
+          default:
+            throw new Error("Faculty already exists.");
+        }
+      }
+
+      throw error;
+    }
   }
 
   async deleteFaculty(id) {
